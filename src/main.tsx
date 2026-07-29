5eb5e95c4006eacd628dba2bf2144e938635ae6e
Ali Areskog <write2ali@gmail.com>
2026-07-28 22:53:08 -0400

    use hash routing, github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { createHashHistory } from '@tanstack/history';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen.ts'
 
-import './main.css';
+// Create the hash history
+const hashHistory = createHashHistory()
 
-// Create a new router instance
-const router = createRouter({ routeTree });
+import './main.css';
+  // Create a new router instance
+const router = createRouter({ routeTree, history: hashHistory });
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
